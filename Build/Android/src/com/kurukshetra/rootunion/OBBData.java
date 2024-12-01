f7387184dc39b7fa2970ec0f9c1f5b330c9c7424
SikandarSdlc <sikandar00824205@gmail.com>
2024-12-02 05:24:06 +0530

    Update OBBData.java

--- Build/Android/src/com/kurukshetra/rootunion/OBBData.java
+++ Build/Android/src/com/kurukshetra/rootunion/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-287730527L // the length of the file in bytes
+117093716L // the length of the file in bytes
 )
 };
 };
